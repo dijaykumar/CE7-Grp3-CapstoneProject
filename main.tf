f8d8bd42dc96e3d77514e797725d0a4af12b2ea9
dijaykumar <dijaykumar@gmail.com>
2024-11-26 10:41:46 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "ce7_grp3_sg" {
 
 # EC2 Instance
 resource "aws_instance" "ce7_grp3_ec2" {
-  ami                    = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI (Amazon Linux 2)
+  ami                    = "ami-0453ec754f44f9a4a"  # Replace with your desired AMI (Amazon Linux 2)
   instance_type          = "t2.micro"
   key_name               = "CE7-Grp3-KeyPair"           # Replace with your SSH key name
   subnet_id              = aws_subnet.ce7_grp3_subnet.id
