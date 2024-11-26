baddac0c37d522e3dac3e1d4cf07453e579633a1
dijaykumar <dijaykumar@gmail.com>
2024-11-26 12:20:06 +0800

    Update install_helloworld.sh

--- scripts/install_helloworld.sh
+++ scripts/install_helloworld.sh
@@ -9,3 +9,34 @@ echo "<html><body><h1>Hello, World!</h1></body></html>" > /var/www/html/index.ht
 # Start Apache
 service httpd start
 chkconfig httpd on
+
+# Install the CloudWatch Agent
+sudo yum install -y amazon-cloudwatch-agent
+
+# Create the CloudWatch Agent config file
+sudo echo '{
+  "metrics": {
+    "metrics_collected": {
+      "mem": {
+        "measurement": [
+          "mem_used",
+          "mem_free",
+          "mem_cached",
+          "mem_total"
+        ],
+        "metrics_collection_interval": 60
+      },
+      "disk": {
+        "measurement": [
+          "disk_used",
+          "disk_free",
+          "disk_total"
+        ],
+        "metrics_collection_interval": 60
+      }
+    }
+  }
+}' > /opt/aws/amazon-cloudwatch-agent/bin/config.json
+
+# Start the CloudWatch Agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a start
